7af9b14060ea04e72388cd12b11f24eb77edb034
Natalia Guerrero <natalia.guerrero@ufl.edu>
2022-04-13 14:47:48 -0400

    updating equations of motion to account for COM

--- orbvel.py
+++ orbvel.py
@@ -1,54 +1,69 @@
 import numpy as np
 from config import *
 
-def f_xy(rs, **kwargs):
+def v_init(r, phi, **kwargs):
     """
-    Calculate f(x,y,t) = x'' for a given [x,y]
-    Inputs:
-        rs, 2-D array [x,y]
-    
-    Returns 2-D array [x'', y'']
-    
-    """
-    
-    x = rs[0]
-    y = rs[1]
-    coeff = G*M/np.pow(x**2 + y**2, 1.5)
-    return coeff*rs
-
-
-def v(rs, **kwargs):
-    """
-    Evaluate first time derivative of r, phi for closed orbits.
+    Evaluate first time derivative of r, phi.
     
     Inputs: r -- 2D vector w/ elems r (separation) 
                  and phi (angle)
     Returns: 2D array of functions v(theta) and v(omega) 
              evaluated at these points
     """
-    r, phi = rs[0], rs[1]
-    vphi = np.sqrt(G*M*p)/r**2  # phi'
-    vr = (e/p)*r**2*np.sin(phi)*vphi
-    return np.array([vr, vphi], float)
+        
+    r0 = r
+    phi0 = -np.arccos((p - r0)/(r0*e))
+    
+    # first derivative of (r0, phi0)
+    fphi0 = np.sqrt(G*M*p)/r0**2 
+    fr = e*r0**2*np.sin(phi0)*fphi0/p
 
-def f(rs, **kwargs):
+    # starting velocity of the second galaxy (cartesian)
+    vx0 = -r0*np.sin(phi0)*fphi0 + fr*np.cos(phi0)
+    vy0 = r0*np.cos(phi0)*fphi0 + fr*np.sin(phi0)
+
+    return np.array([vx0, vy0], float)
+
+def f(r, **kwargs):
     """
-    Function defining system of equations of motion for two bodies
-      orbiting about a center of mass
+    Evaluate acceleration of a body with respect to the center of mass
+      with another body, f(x), f(y)
+      
+    Inputs: 
+        Pair of coordinates [x,y]
     
-    Inputs: r -- 2D vector w/ elems r (distance from center of mass),
-                    phi (angle from distance of closest approach)
-    Returns: 2D array of functions f(theta) and f(phi) 
-             evaluated at r, phi
+    Returns:
+        Array, [f(x), f(y)]
     """
-    r, phi = rs[0], rs[1]
-    vr, vphi = v(rs)
-    #fphi = np.sqrt(G*M*p)/r**2  # this is actually phi'
-    fphi = -2*np.sqrt(G*M*p)*r**-3*vr  # f(phi) = phi''
-    # fr = ecc*r**2*np.sin(phi)*fphi/p
-    fr = e*np.sqrt(G*M/p)*np.cos(phi)*vphi  # f(r) = r''
+        
+    x, y = rs[0], rs[1]  
+    r = np.sqrt(x**2 + y**2)
     
-    return np.array([fr, fphi], float)
+    # x, y components of Acceleration 'a'
+    ax = G*M*x/r**3    # m_rest: mass of the body at rest
+    ay = G*M*y/r**3    
+    
+    return np.array([ax, ay], float)
+
+
+# def f(rs, **kwargs):
+#     """
+#     Function defining system of equations of motion for two bodies
+#       orbiting about a center of mass
+    
+#     Inputs: r -- 2D vector w/ elems r (distance from center of mass),
+#                     phi (angle from distance of closest approach)
+#     Returns: 2D array of functions f(theta) and f(phi) 
+#              evaluated at r, phi
+#     """
+#     r, phi = rs[0], rs[1]
+#     vr, vphi = v(rs)
+#     #fphi = np.sqrt(G*M*p)/r**2  # this is actually phi'
+#     fphi = -2*np.sqrt(G*M*p)*r**-3*vr  # f(phi) = phi''
+#     # fr = ecc*r**2*np.sin(phi)*fphi/p
+#     fr = e*np.sqrt(G*M/p)*np.cos(phi)*vphi  # f(r) = r''
+    
+#     return np.array([fr, fphi], float)
 
 # def f(r, **kwargs):
 #     """
--- rungekutte.py
+++ rungekutte.py
@@ -1,13 +1,43 @@
 import numpy as np
 from config import *
 
-def rk4(m0, m1, t0, tmax, r0, phi0, f, h, dr_tol, r_ref):
+def get_path_err(Ri):
+    """
+    Get analytical solution of galaxy path for pair of coordinates, [x, y].
+    
+    The analytic solution of an ellipse is:
+        (x**2 / a**2) + (y**2 / b**2) = 1
+    
+    We calculate the left-hand side of the equation, and
+      return the error = (1 - LHS).
+    
+    """
+    
+    x, y = Ri 
+    
+    # parabolic case
+    if e == 1.:
+        r_test = np.sqrt(x**2 + y**2)
+        phi_test = np.tan(x / y)
+        
+        r_true = 2*rmin / (1 + np.cos(phi))
+    
+    # closed orbit case
+    else:
+        b = a * np.sqrt(1 - e**2)
+        test_point = (x**2 / a**2) + (y**2 / b**2)
+        err = 1 - test_point
+
+
+    return err
+    
+
+def rk4(t0, tmax, r0, phi0, f, h, dr_tol, r_ref):
     """
     Calculate orbit of a body about center of mass using 4th-order Runge-Kutte
     method.
     
     Inputs:
-      m0, m1: mass of each body, kg
       t0, tmax: start, end time of simulation, s
       r0: initial position of second body, m
       phi0: initial angular position of second body, rad
@@ -16,10 +46,16 @@ def rk4(m0, m1, t0, tmax, r0, phi0, f, h, dr_tol, r_ref):
       dr_tol: amount of allowed deviation from r_ref
       r_ref: expected value for r
     
+    Returns:
+      R: 2-D array of x,y coordinates
+      err: relative error of each (x,y) pair
+      
     """
     
     # initial conditions
-    R0 = [r0, phi0]
+    x0 = r0 * np.cos(phi0)
+    y0 = r0 * np.cos(phi0)
+    R0 = [x0, y0]
 
     # initialize arrays for t, orbital elements
     t = np.array(t0)
@@ -27,13 +63,13 @@ def rk4(m0, m1, t0, tmax, r0, phi0, f, h, dr_tol, r_ref):
     Ri = np.array(R0)
     R = np.array([Ri])
     
-    debug = False
+    debug = True
     
     # break if r goes outside tolerance
-    r_check = Ri[0]
     err = np.array([])
+    err_i = get_path_err(Ri)
     
-    while np.abs(r_check - r_ref) < dr_tol:
+    while err_i < dr_tol:
         # break if t = tmax
         if ti > tmax:
             if debug: print(f"Time: {ti} > {tmax}")
@@ -41,7 +77,7 @@ def rk4(m0, m1, t0, tmax, r0, phi0, f, h, dr_tol, r_ref):
         else:
             # calculate y(t), y'(t)
             if debug: print(ti, Ri, np.abs(r_check - r_ref))
-            err = np.append(err, (r_ref - r_check)/r_ref)
+            err = np.append(err, get_path_err(Ri))
             
             # compute k1, k2, k3, k4 for Ri (r_i, phi_i, r_i', phi_i')
             k1 = h*f(Ri)
@@ -54,7 +90,7 @@ def rk4(m0, m1, t0, tmax, r0, phi0, f, h, dr_tol, r_ref):
             Ri += (1/6)*(k1 + 2*k2 + 2*k3 + k4)
 
             # update r_check
-            r_check = Ri[0]
+            r_check = Ri
 
             # save new ri to r
             t = np.append(t, ti)
@@ -65,4 +101,4 @@ def rk4(m0, m1, t0, tmax, r0, phi0, f, h, dr_tol, r_ref):
 #     x = R[:,0]*np.cos(R[:,1])  # r * cos phi
 #     y = R[:,0]*np.sin(R[:,1])  # r * sin phi
 
-    return R, x, y, err
+    return R, err
